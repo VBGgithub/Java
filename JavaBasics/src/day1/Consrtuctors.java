d71133abb8c390ef5a74a906f8e926cdf6150e4e
vrinda <vrindajagtap@gmail.com>
2022-05-05 17:27:39 +0530

    First Commit

---
+++ JavaBasics/src/day1/Consrtuctors.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Consrtuctors {
+public static void main(String[] args) {
+	
+	int i=100;
+	Integer x=new Integer(i);
+	
+	String str= "abc";
+	
+	Integer y=new Integer(str);
+}
+}
